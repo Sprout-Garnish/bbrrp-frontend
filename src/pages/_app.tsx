80c4b6ada4780a1257f2083617b4d92cf3f2b37f
01Joseph-Hwang10 <joseph95501@gmail.com>
2022-03-13 15:24:25 +0900

    setup react query and framer motion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
-import React from "react";
+import React, { useState } from "react";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // 서버사이드 렌더링으로 인한 오류를 방지하기 위해 사용
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
---
+++ src/utilities/types.ts
@@ -0,0 +1,3 @@
+// 이 파일은 Utility 타입을 저장하기 위한 파일입니다
+
+export type Nullable<T = any> = T | null | undefined;
