e9914bba1dbb77d5a958202754b3a4f860330323
01Joseph-Hwang10 <joseph95501@gmail.com>
2022-05-06 16:45:33 +0900

    add useLogin

--- modules/client/hooks/useLogin.ts
+++ modules/client/hooks/useLogin.ts
@@ -8,21 +8,18 @@ import {
 } from "../interfaces/AuthMutation";
 import { isLoginSuccess } from "../shared/helpers";
 
-/**
- * @description THIS HOOK IS DEV ONLY
- */
-export const useDevLogin = () => {
+export const useLogin = () => {
   const { setLoggedIn } = useContext(BBRRPClientContext);
   const [authMutation, { loading, data: result }] = useMutation<
     AuthMutation,
     AuthMutationVariables
   >(AUTH_MUTATION);
 
-  const login = async () => {
-    const Credentials = require("@private/credentials");
+  const login = async (email: string, password: string) => {
     const res = await authMutation({
       variables: {
-        ...Credentials,
+        email,
+        password,
       },
     });
     const success = isLoginSuccess(res?.data);
