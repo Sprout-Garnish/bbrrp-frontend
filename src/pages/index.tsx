39597e051d74154cb68e418cda3061d073e49289
황현규 <joseph95501@gmail.com>
2022-05-06 16:47:02 +0900

    Revert "useDevLogin"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import type { NextPage } from "next";
-import RestaurantList from "@src/components/home/restaurant-list";
 
 const Home: NextPage = () => {
-  return <RestaurantList />;
+  return <>content</>;
 };
 
 export default Home;
