c9be7421161526dd71e1ef389f7e234f68339ed8
taeju-moon <pp2lycee@naver.com>
2022-06-30 15:13:32 +0900

    commit to change branch

---
+++ src/components/common/loginButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginButton: React.FC = () => {
+  return (
+    <>
+      <button className="btn btn-primary">Login</button>
+    </>
+  );
+};
+
+export default LoginButton;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import type { NextPage } from "next";
 import RestaurantList from "@src/components/home/restaurant-list";
+import LoginButton from "@src/components/common/loginButton";
 
 const Home: NextPage = () => {
-  return <RestaurantList />;
+  return (
+    <>
+      <RestaurantList />
+      <LoginButton />
+    </>
+  );
 };
 
 export default Home;
