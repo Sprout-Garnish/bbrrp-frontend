cbb921314006608acf55d3045cae4eabac096ebe
01Joseph-Hwang10 <joseph95501@gmail.com>
2022-07-12 14:19:03 +0900

    add colors to tailwindcss

---
+++ modules/theme/color.js
@@ -0,0 +1,13 @@
+export const ThemeColor = {
+  // Lemon table - #ffe12e
+  lemon: "#ffe12e",
+
+  // Restaurant reserve website - #d3d2c1
+  whitesmoke: "#d3d2c1",
+
+  // Product/ introduce/ image- #60603c
+  khaki: "#60603c",
+
+  // Table/ review/ location- #ffb42e
+  orange: "#ffb42e",
+}
